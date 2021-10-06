eacf74722ed55fa329168ffebfbeb1ad9ce7a994
GavryaAK <alina.gavrya@gmail.ru>
2021-10-06 21:54:23 +0300

    добавила ожидание ввода через std::promise, ввод перестал работать

--- cppsrc/client/Client.cpp
+++ cppsrc/client/Client.cpp
@@ -43,6 +43,19 @@ Napi::Value Client::Connect(const Napi::CallbackInfo& info) {
 	return Napi::Boolean::New(info.Env(), main_socket.Connect(Endpoint(IP, PORT)));
 }
 
+std::thread promise_thread;
+using Context = Napi::Reference<Napi::Value>;
+using DataType = std::promise<std::string>;
+void CallJs(Napi::Env env, Napi::Function jsCallback, Context* context, DataType* message) {
+	// Transform native data into JS data, passing it to the provided
+	// `jsCallback` -- the TSFN's JavaScript function.
+	jsCallback.Call(context->Value(), { Napi::String::New(env, "help") });
+	delete message;
+};
+using TSFN = Napi::TypedThreadSafeFunction<Context, DataType, CallJs>;
+TSFN promise_tsfn;
+std::promise<std::string> promise;
+
 std::thread native_thread;
 Napi::ThreadSafeFunction tsfn;
 
@@ -92,7 +105,7 @@ Napi::Value Client::StartChating(const Napi::CallbackInfo& info) {
 				std::cout << *message << std::endl;
 
 				// dont work
-				napi_status status = tsfn.BlockingCall(message, callback);
+				napi_status status = tsfn.NonBlockingCall(message, callback);
 				if (status != napi_ok) {
 					//Napi::Error::Fatal(
 					//	"ThreadEntry",
@@ -106,23 +119,55 @@ Napi::Value Client::StartChating(const Napi::CallbackInfo& info) {
 	);
 	//native_thread.join();
 	//native_thread.detach();
+
 	Napi::Function input = info[1].As<Napi::Function>();
-	std::string send_msg;
-	while (true) {
+
+	//Context* context = new Napi::Reference<Napi::Value>(Persistent(info.This()));
+
+	//promise_tsfn = TSFN::New(
+	//	env,							// Environment
+	//	info[0].As<Napi::Function>(),	// JS function from caller
+	//	"TSFN",							// Resource name
+	//	0,								// Max queue size (0 = unlimited).
+	//	1,								// Initial thread count
+	//	context,
+	//	[](Napi::Env, void*, Context* ctx) {        // Finalizer used to clean threads up
+	//		native_thread.join();
+	//		delete ctx;
+	//	}
+	//);
+	//promise_thread = std::thread(
+	//	[this]() {
+	//		while (true) {
+	//			std::promise<std::string> promise;
+	//			auto future = promise.get_future();
+	//			promise_tsfn.NonBlockingCall(&promise);
+	//			auto result = future.get();
+	//		}
+	//		tsfn.Release();
+	//	}
+	//);
+	//std::string send_msg;
+
+	//while (true) {
 		auto p = input.Call({}).As<Napi::Object>();
 		auto thenFunc = p.Get("then").As<Napi::Function>();
+		
+		auto future = promise.get_future();
 		thenFunc.Call(p, { Napi::Function::New(env, [this](const Napi::CallbackInfo& info) {
 			std::string result = info[0].As<Napi::String>().Utf8Value();
 			std::cout << result << std::endl;
-			if (main_socket.Send(result) == Result::Error) {
-				int error = WSAGetLastError();
-				//return Napi::Boolean::New(info.Env(), false);
-			}
+			promise.set_value(result);
 			}) });
+		std::string result = future.get();
+		if (main_socket.Send(result) == Result::Error) {
+			int error = WSAGetLastError();
+			return Napi::Boolean::New(info.Env(), false);
+		}
 		//send_msg = input.Call(info.Env().Global(), {}).As<Napi::String>();
 		//std::getline(std::cin, send_msg);
 		
-	}
+	//}
 	return Napi::Boolean::New(info.Env(), false);
 }
 
